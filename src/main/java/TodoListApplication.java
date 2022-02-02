39f22a71013eac717b81702b992cca02f51f527d
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2022-02-02 21:15:28 +0200

    setup

---
+++ src/main/java/TodoListApplication.java
@@ -0,0 +1,11 @@
+public class TodoListApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to ToDo List!");
+        System.out.println("Please make a choice:");
+
+        System.out.println("1. Add");
+        System.out.println("2. Get All");
+        System.out.println("3. Delete");
+    }
+}
