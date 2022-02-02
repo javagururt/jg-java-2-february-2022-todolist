9c379c0a321e9fa868c263f090d81a824a4729ba
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2022-02-02 21:39:40 +0200

    feature/1 add todo list, get todo list

---
+++ src/main/java/ToDoEntity.java
@@ -0,0 +1,56 @@
+import java.util.Objects;
+
+class ToDoEntity {
+
+    private int id;
+    private String name;
+    private String description;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ToDoEntity that = (ToDoEntity) o;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
--- src/main/java/TodoListApplication.java
+++ src/main/java/TodoListApplication.java
@@ -1,11 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class TodoListApplication {
 
     public static void main(String[] args) {
         System.out.println("Welcome to ToDo List!");
         System.out.println("Please make a choice:");
 
-        System.out.println("1. Add");
-        System.out.println("2. Get All");
-        System.out.println("3. Delete");
+        int todoSequenceId = 0;
+        Map<Integer, ToDoEntity> repository = new HashMap<>();
+
+        while (true) {
+            var scanner = new Scanner(System.in);
+            System.out.println("1. Add");
+            System.out.println("2. Get All");
+            System.out.println("3. Delete");
+
+            var userInput = Integer.parseInt(scanner.nextLine());
+
+            if (userInput == 1) {
+                System.out.println("Please enter todo name:");
+                var name = scanner.nextLine();
+                System.out.println("Please enter todo description:");
+                var description = scanner.nextLine();
+                var todo = new ToDoEntity();
+                todo.setName(name);
+                todo.setDescription(description);
+
+                todo.setId(todoSequenceId);
+                todoSequenceId++;
+
+                repository.put(todo.getId(), todo);
+            } else if (userInput == 2) {
+                repository.entrySet().stream()
+                        .map(Map.Entry::getValue)
+                        .forEach(System.out::println);
+            }
+        }
     }
 }
