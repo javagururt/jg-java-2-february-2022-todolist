2a7ede922363c6d35c66539fa84ebde467aab9cd
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2022-02-02 21:45:26 +0200

    feature/1 added try/catch

--- src/main/java/TodoListApplication.java
+++ src/main/java/TodoListApplication.java
@@ -12,30 +12,34 @@ public class TodoListApplication {
         Map<Integer, ToDoEntity> repository = new HashMap<>();
 
         while (true) {
-            var scanner = new Scanner(System.in);
-            System.out.println("1. Add");
-            System.out.println("2. Get All");
-            System.out.println("3. Delete");
+            try {
+                var scanner = new Scanner(System.in);
+                System.out.println("1. Add");
+                System.out.println("2. Get All");
+                System.out.println("3. Delete");
 
-            var userInput = Integer.parseInt(scanner.nextLine());
+                var userInput = Integer.parseInt(scanner.nextLine());
 
-            if (userInput == 1) {
-                System.out.println("Please enter todo name:");
-                var name = scanner.nextLine();
-                System.out.println("Please enter todo description:");
-                var description = scanner.nextLine();
-                var todo = new ToDoEntity();
-                todo.setName(name);
-                todo.setDescription(description);
+                if (userInput == 1) {
+                    System.out.println("Please enter todo name:");
+                    var name = scanner.nextLine();
+                    System.out.println("Please enter todo description:");
+                    var description = scanner.nextLine();
+                    var todo = new ToDoEntity();
+                    todo.setName(name);
+                    todo.setDescription(description);
 
-                todo.setId(todoSequenceId);
-                todoSequenceId++;
+                    todo.setId(todoSequenceId);
+                    todoSequenceId++;
 
-                repository.put(todo.getId(), todo);
-            } else if (userInput == 2) {
-                repository.entrySet().stream()
-                        .map(Map.Entry::getValue)
-                        .forEach(System.out::println);
+                    repository.put(todo.getId(), todo);
+                } else if (userInput == 2) {
+                    repository.entrySet().stream()
+                            .map(Map.Entry::getValue)
+                            .forEach(System.out::println);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
