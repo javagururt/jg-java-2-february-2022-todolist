f27f7b2cf39337052605a52e51a6ac1f060abb04
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2022-04-23 21:40:48 +0300

    removed unused annotation

--- src/main/java/com/javaguru/todolist/domain/ToDoEntity.java
+++ src/main/java/com/javaguru/todolist/domain/ToDoEntity.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -31,7 +30,6 @@ public class ToDoEntity {
 //    @JoinColumn(name = "user_id", referencedColumnName = "id")
 //    private UserEntity userEntity;
     @Column(name = "user_id")
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private Integer userId;
 
 }
